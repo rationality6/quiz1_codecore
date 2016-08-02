eef0c8698e72f66f12b7f49e58aa91398b13846e
Hyun Ahn <rationality6@gmail.com>
2016-08-02 13:55:55 -0700

    Finish really.

--- bonus.rb
+++ bonus.rb
@@ -1,31 +1,10 @@
-# module HelperMethods
-#     def self.titleize(string)
-#         # rule0 = string == 'and' || string == 'or' || string == 'in' || string == 'from' || string == 'the' || string == 'of'
-#         rule0 = -> (x) { x == 'and' }
-#
-#         p string.split.collect{|x| true ? x : x.capitalize}.join(' ')
-#     end
-# end
-#
-# HelperMethods.titleize('hello world in the of and or from')
-# # p HelperMethods.titleize('I like coding')
-
 module HelperMethods
     def self.titleize(string)
-        result_array = []
-        splited_strings = string.split(' ')
-        for string in splited_strings
-            if string == 'and' || string == 'or' || string == 'in' || string == 'from' || string == 'the' || string == 'of'
-                result_array << string
-            else
-                result_array << string.capitalize
-            end
-        end
-        result_array.join(" ")
+        string.gsub(/\b(?!and)(?!or)(?!in)(?!from)(?!the)(?!of)[a-z]/) { $&.capitalize }
     end
 end
 
-if __FILE__ ==$0
-    p HelperMethods.titleize('hello world in the of and or from')
-    p HelperMethods.titleize('I like coding')
+if __FILE__ == $PROGRAM_NAME
+    p HelperMethods.titleize('i like running and coding 23 r4t4rt3 3 or')
+    p HelperMethods.titleize('i like or and coding 23 r4t4rt3 3 or')
 end
