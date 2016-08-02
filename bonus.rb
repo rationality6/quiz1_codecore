56c2c25f127e36d96528982d9d8f8fc64b149313
Hyun Ahn <rationality6@gmail.com>
2016-08-02 13:04:43 -0700

    No mutation.

--- bonus.rb
+++ bonus.rb
@@ -1,11 +1,31 @@
+# module HelperMethods
+#     def self.titleize(string)
+#         # rule0 = string == 'and' || string == 'or' || string == 'in' || string == 'from' || string == 'the' || string == 'of'
+#         rule0 = -> (x) { x == 'and' }
+#
+#         p string.split.collect{|x| true ? x : x.capitalize}.join(' ')
+#     end
+# end
+#
+# HelperMethods.titleize('hello world in the of and or from')
+# # p HelperMethods.titleize('I like coding')
+
 module HelperMethods
     def self.titleize(string)
-        # rule0 = string == 'and' || string == 'or' || string == 'in' || string == 'from' || string == 'the' || string == 'of'
-        rule0 = -> (x) { x == 'and' }
-
-        puts string.split.collect{|x| rule0  ? x : x.capitalize}.join(' ')
+        result_array = []
+        splited_strings = string.split(' ')
+        for string in splited_strings
+            if string == 'and' || string == 'or' || string == 'in' || string == 'from' || string == 'the' || string == 'of'
+                result_array << string
+            else
+                result_array << string.capitalize
+            end
+        end
+        result_array.join(" ")
     end
 end
 
-HelperMethods.titleize('hello world in the of and or from')
-# p HelperMethods.titleize('I like coding')
+if __FILE__ ==$0
+    p HelperMethods.titleize('hello world in the of and or from')
+    p HelperMethods.titleize('I like coding')
+end
